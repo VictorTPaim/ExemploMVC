72d5d4f834735e76a863f421e6b9307e60387d1d
Victor Paim <victortpaim@hotmail.com>
2022-08-17 09:20:21 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ExemploAspNetMVC.Models;
+
+namespace ExemploAspNetMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View(); //view ta dentro da classe Controller
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult PrimeiraAction()
+    {
+        return View();
+    }
+
+    public string TesteQueryString([FromQuery] string q, [FromQuery] string nome) 
+    {
+        return $"Chegou Aqui {q} e {nome}";
+    }
+
+    public string TesteForm([FromQuery] string nome, [FromQuery] string email)
+    {
+        return $"Nome: {nome}, Email: {email}";
+    }
+
+    public IActionResult Form()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"); //o igual é o padrao caso o usuario nao passe nada
+
+app.Run();
